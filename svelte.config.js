6547b065e88092f89d0347a5a778132f1fabfdaf
yifan117 <yifan.qiu17@gmail.com>
2022-09-16 20:22:54 +0930

    changed deploy settings

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
 import adapter from '@sveltejs/adapter-netlify';
-import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: preprocess(),
 
     kit: {
-        adapter: adapter({ edge: true })    // edge fix
+        target: '#svelte',
+        adapter: adapter()    // edge fix
     }
 };
 
